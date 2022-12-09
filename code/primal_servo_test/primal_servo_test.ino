f160683426c74da0022ad52635f14d01e6c9c771
AnMe3z <prodeadlight@gmail.com>
2022-12-09 23:07:29 +0200

    all leg parts

---
+++ code/primal_servo_test/primal_servo_test.ino
@@ -0,0 +1,48 @@
+/* Include the HCPCA9685 library */
+#include "HCPCA9685.h"
+
+/* I2C slave address for the device/module. For the HCMODU0097 the default I2C address
+   is 0x40 */
+#define  I2CAdd 0x40
+
+
+/* Create an instance of the library */
+HCPCA9685 HCPCA9685(I2CAdd);
+
+
+void setup() 
+{
+  /* Initialise the library and set it to 'servo mode' */ 
+  HCPCA9685.Init(SERVO_MODE);
+
+  /* Wake the device up */
+  HCPCA9685.Sleep(false);
+}
+
+
+void loop() 
+{
+  unsigned int Pos;
+  int minDeg = 200;
+  int maxDeg = 300;
+
+  /* Sweep the servo back and forth from its minimum to maximum position.
+     If your servo is hitting its end stops then you  should adjust the 
+     values so that the servo can sweep though its full range without hitting
+     the end stops. You can adjust the min & max positions by altering 
+     the trim values in the libraries HCPCA9685.h file*/
+  for(Pos = minDeg; Pos < maxDeg; Pos++)
+  {
+    /* This function sets the servos position. It takes two parameters, 
+     * the first is the servo to control, and the second is the servo 
+     * position. */
+    HCPCA9685.Servo(3, Pos);
+    delay(10);
+  }
+  
+  for(Pos = maxDeg; Pos >= minDeg; Pos--)
+  {
+    HCPCA9685.Servo(3, Pos);
+    delay(10);
+  }
+}
