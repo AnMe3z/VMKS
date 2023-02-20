163895281b5cc49305b8f96b9a6a7f49dbd30e42
Viktorio <viktorio.k.milanov.2019@elsys-bg.org>
2023-02-20 17:11:18 +0200

    working properly

--- code/esp-test/sketch_jan18a/sketch_jan18a.ino
+++ code/esp-test/sketch_jan18a/sketch_jan18a.ino
@@ -21,7 +21,7 @@ void loop()
 
    if(Serial.available() > 0){
       receivedData = Serial.read();
-      if(receivedData == 48){
+      if(receivedData == (char)49){
         for(int i = 65; i <= 90; i++) {
           Serial.print((char)i);
           delay(50);
@@ -31,13 +31,4 @@ void loop()
 
    delay(200);
 
-   
-  if (Serial.available()) {
-    SerialBT.write(Serial.read());
-  }
-  if (SerialBT.available()) {
-    Serial.write(SerialBT.read());
-  }
-  delay(20);
-    
 }
