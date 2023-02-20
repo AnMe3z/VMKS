5c1f47c9d5b6ed44a95f7adce0ec0dce674eeb01
Viktorio <viktorio.k.milanov.2019@elsys-bg.org>
2023-02-20 18:20:18 +0200

    ALMOST working BT connectivity

--- code/app/app/src/main/java/com/example/vmksapp/MainActivity.java
+++ code/app/app/src/main/java/com/example/vmksapp/MainActivity.java
@@ -5,12 +5,21 @@ import androidx.core.app.ActivityCompat;
 
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
+
+    static final UUID mUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     @SuppressLint("MissingPermission")
     @Override
@@ -22,10 +31,61 @@ public class MainActivity extends AppCompatActivity {
 
         BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
         System.out.println(btAdapter.getBondedDevices());
+
+        BluetoothDevice esp32 = btAdapter.getRemoteDevice("78:21:84:E0:B8:CA");
+        System.out.println(esp32.getName());
+
+        BluetoothSocket btSocket = null;
+        int counter = 0;
+        do {
+            try {
+                btSocket = esp32.createRfcommSocketToServiceRecord(mUUID);
+                System.out.println(btSocket);
+                btSocket.connect();
+                System.out.println(btSocket.isConnected());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            counter++;
+        } while (!btSocket.isConnected() && counter < 3);
+
+
+        try {
+            OutputStream outputStream = btSocket.getOutputStream();
+            outputStream.write(49);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        InputStream inputStream = null;
+        try {
+            inputStream = btSocket.getInputStream();
+            inputStream.skip(inputStream.available());
+
+            for (int i = 0; i < 26; i++) {
+
+                byte b = (byte) inputStream.read();
+                System.out.println((char) b);
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            btSocket.close();
+            System.out.println(btSocket.isConnected());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    //[E0:62:67:47:19:AD, 55:0F:63:31:54:3D, FC:58:FA:2F:4C:2C, AA:F0:36:2F:B6:63, 80:57:19:E8:43:8F, 78:21:84:E0:B8:CA, 41:42:4C:F8:B4:A0, 58:FB:84:86:29:04, FF:FF:38:59:B5:B3, FC:58:FA:4B:E8:7F]
-    //[E0:62:67:47:19:AD, 55:0F:63:31:54:3D, FC:58:FA:2F:4C:2C, AA:F0:36:2F:B6:63, 80:57:19:E8:43:8F, 41:42:4C:F8:B4:A0, 58:FB:84:86:29:04, FF:FF:38:59:B5:B3, FC:58:FA:4B:E8:7F]
-    //78:21:84:E0:B8:CA
+    //All MACs (with ESP):[E0:62:67:47:19:AD, 55:0F:63:31:54:3D, FC:58:FA:2F:4C:2C, AA:F0:36:2F:B6:63, 80:57:19:E8:43:8F, 78:21:84:E0:B8:CA, 41:42:4C:F8:B4:A0, 58:FB:84:86:29:04, FF:FF:38:59:B5:B3, FC:58:FA:4B:E8:7F]
+    //Without ESP: [E0:62:67:47:19:AD, 55:0F:63:31:54:3D, FC:58:FA:2F:4C:2C, AA:F0:36:2F:B6:63, 80:57:19:E8:43:8F, 41:42:4C:F8:B4:A0, 58:FB:84:86:29:04, FF:FF:38:59:B5:B3, FC:58:FA:4B:E8:7F]
+    //ESP MAC: 78:21:84:E0:B8:CA
+
     public void initButtons() {
         createButtonListener((Button) findViewById(R.id.button1));
         createButtonListener((Button) findViewById(R.id.button2));
