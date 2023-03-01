6ad5bef7d4db7ed5c23cb1d24996ba45296d0724
AnMe3z <prodeadlight@gmail.com>
2023-03-01 14:21:30 +0200

    improvements

--- code/stage1/servos/main/main.ino
+++ code/stage1/servos/main/main.ino
@@ -12,59 +12,67 @@ HCPCA9685 HCPCA9685(I2CAdd);
 int servoRoms[4][6] = {{40, 180, 90, 170, 0, 0}, {3, 120, 85, 156, 0, 0}, {0, 115, 90, 150, 0, 0}, {0, 115, 100, 180, 0, 0}};
 int leg[4][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {9, 10, 11}};
 
-// kak bi rabotil mapinga 
-// Servo.write(map(angle, 0, 180, servo-roms[0][0], servo-roms[0][1]));
-
 int legLenght = 10;
 
-double torso = 2.0;
-bool bodyUp;
+double targetAngle; // real targeted angle: maxAngle - targetAngle
+bool torsoMoving;
 
 void setup() {
+  //init the servo driver
   HCPCA9685.Init(SERVO_MODE);
   HCPCA9685.Sleep(false);
 
-  inverseKinematics(0, 1);
-  inverseKinematics(1, 1);
-  inverseKinematics(2, 1);
-  inverseKinematics(3, 1);
-  bodyUp = true;
-  
+  //what i want:
+  // - when the program starts move the body to lowered position
+  // - stand up
+
+  targetAngle = 1;
+
+  // init pause
   delay(3000);
 }
 
 void loop() {
-  
-  inverseKinematics(0, 12);
-  inverseKinematics(1, 12);
-  inverseKinematics(2, 11);
-  inverseKinematics(3, 11);
-  delay(3000);
-  
-  inverseKinematics(0, 1);
-  inverseKinematics(1, 1);
-  inverseKinematics(2, 1);
-  inverseKinematics(3, 1);
+
+  inverseKinematicsDelayed(0, 0);
+  inverseKinematicsDelayed(1, 0);
+  inverseKinematicsDelayed(2, 0);
+  inverseKinematicsDelayed(3, 0);
   delay(3000);
 
-  // --- up and down loop code
-//  if(bodyUp){
-//    torso += 0.2;
-//    if(torso == 12.0){
-//      bodyUp = false;
-//    }
-//  }
-//  else{
-//    torso -= 0.2;
-//    if(torso == 2.0){
-//      bodyUp = true;
-//    }
-//  }
-//  
-//  inverseKinematics(0, torso);
-//  inverseKinematics(1, torso);
-//  inverseKinematics(2, torso);
-//  inverseKinematics(3, torso);
+  inverseKinematicsDelayed(0, 12);
+  inverseKinematicsDelayed(1, 12);
+  inverseKinematicsDelayed(2, 11);
+  inverseKinematicsDelayed(3, 11);
+  delay(3000);
+}
+
+void inverseKinematicsDelayed(int legIndex, double torsoHeight){
+  // --- improved version of inverseKinematics() ---
+  // provides smooth movement to the calculated target angle
+  
+  int a = legLenght;
+  int b = a;
+  double c = torsoHeight;
+
+  int kneeServoIndex = leg[legIndex][0];
+  int kneeMin = servoRoms[legIndex][0];
+  int kneeAngle = cosineTheorem(a, b, c)/2;
+  int femurServoIndex = leg[legIndex][1];
+  int femurMin = servoRoms[legIndex][2];
+  int femurAngle = cosineTheorem(a, c, b);
+
+  //knee
+  for (int i = kneeMin; i <= kneeAngle; i++) {
+    HCPCA9685.Servo(kneeServoIndex, map(i, 0, 180, 1, 450));
+    delay(50);
+  }
+  
+  //femur
+  for (int i = femurMin; i <= femurAngle; i++) {
+    HCPCA9685.Servo(femurServoIndex, map(i, 0, 180, 1, 450));
+    delay(50);
+  }
 }
 
 void inverseKinematics(int legIndex, double torsoHeight){
@@ -77,15 +85,20 @@ void inverseKinematics(int legIndex, double torsoHeight){
   int kneeAngle = cosineTheorem(a, b, c)/2;
   int femurServoIndex = leg[legIndex][1];
   int femurMin = servoRoms[legIndex][2];
-  int femurMax = servoRoms[legIndex][3];
   int femurAngle = cosineTheorem(a, c, b);
-  
+
   //knee
   HCPCA9685.Servo(kneeServoIndex, map(kneeMin + kneeAngle, 0, 180, 1, 450));
+  
   //femur
   HCPCA9685.Servo(femurServoIndex, map(femurMin + femurAngle, 0, 180, 1, 450));
 }
 
 double cosineTheorem(int a, int b, double c){
-  return acos((b * b + a * a - c * c)/(2.0 * b * a)) * 57296 / 1000;
+  if (c == 0){
+    return 0;
+  }
+  else{
+    return acos((b * b + a * a - c * c)/(2.0 * b * a)) * 57296 / 1000;
+  }
 }
