209d03046647600c3e456f14c78d9abf4b26b373
AnMe3z <prodeadlight@gmail.com>
2023-03-04 08:52:26 +0200

    fixed the normal ik

--- code/stage1/servos/main/main.ino
+++ code/stage1/servos/main/main.ino
@@ -10,6 +10,7 @@ HCPCA9685 HCPCA9685(I2CAdd);
 //int servoRoms[12][12] = {{72, 180}, {22, 170}, {0, 0}, {10, 140}, {0, 180}, {0, 0}, {0, 70}, {35, 70}, {0, 0}, {10, 60}, {35, 140}, {0, 0}};
 //                      c             c           c            c           c           c           c             c
 int servoRoms[4][6] = {{40, 180, 90, 170, 0, 0}, {3, 120, 85, 156, 0, 0}, {0, 115, 90, 150, 0, 0}, {0, 115, 100, 180, 0, 0}};
+//the legs need to be closed at the start of the program for the angles to be true
 int servoCurrentAngles[4][3] = {{40, 170, 0}, {3, 156, 0}, {0, 150, 0}, {0, 180, 0}};
 int leg[4][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {9, 10, 11}};
 
@@ -76,6 +77,9 @@ void inverseKinematicsDelayed(int legIndex, double torsoHeight){
     // therefore its closing knee angle is 40 instead of way lower angle like the others
     // this causes problems in lower torso hights and prevent the leg from moving at all
     // --- the code bellow is not optimized and it has to be deleted when the leg 0 is physically fixed
+
+    // TODO: figure out why leg 0 knee servo is closing instead of opeing during the delayed ik
+    
     if (legIndex == 0 && kneeAngle < 45) {
       int kneeDirectionAngle;
       kneeDirectionAngle = -1;
@@ -132,13 +136,14 @@ void inverseKinematics(int legIndex, double torsoHeight){
   int kneeAngle = cosineTheorem(a, b, c)/2;
   int femurServoIndex = leg[legIndex][1];
   int femurMin = servoRoms[legIndex][2];
+  int femurMax = servoRoms[legIndex][3];
   int femurAngle = cosineTheorem(a, c, b);
 
   //knee
   HCPCA9685.Servo(kneeServoIndex, map(kneeMin + kneeAngle, 0, 180, 1, 450));
   
   //femur
-  HCPCA9685.Servo(femurServoIndex, map(femurMin + femurAngle, 0, 180, 1, 450));
+  HCPCA9685.Servo(femurServoIndex, map(femurMax - femurAngle, 0, 180, 1, 450));
 }
 
 double cosineTheorem(int a, int b, double c){
