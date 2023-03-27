3c150905fa9597bb6c411ccbee124f202154e929
Viktorio <viktorio.k.milanov.2019@elsys-bg.org>
2023-03-27 18:10:50 +0300

    WORKING FINALLYgit add .git add .git add .!

--- code/app/app/src/main/java/com/example/vmksapp/MainActivity.java
+++ code/app/app/src/main/java/com/example/vmksapp/MainActivity.java
@@ -24,14 +24,17 @@ public class MainActivity extends AppCompatActivity {
     @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("test");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("test2");
 
-       BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
-       System.out.println(btAdapter.getBondedDevices());
-       BluetoothDevice esp32 = btAdapter.getRemoteDevice("78:21:84:E0:B8:CA");        
-       System.out.println(esp32.getName());
-
+        BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+        System.out.println(btAdapter.getBondedDevices());
+//
+        BluetoothDevice esp32 = btAdapter.getRemoteDevice("78:21:84:E0:B8:CA");
+        System.out.println(esp32.getName());
+//
         BluetoothSocket btSocket = null;
         int counter = 0;
         do {
@@ -46,31 +49,21 @@ public class MainActivity extends AppCompatActivity {
 
             counter++;
         } while (!btSocket.isConnected() && counter < 3);
-
-
+//
+//
         try {
             OutputStream outputStream = btSocket.getOutputStream();
-            System.out.println("bruh");
+            System.out.println("test3");
             outputStream.write(49);
-
-
             initButtons(outputStream);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        InputStream inputStream = null;
+//
+        InputStream inputStream;
         try {
             inputStream = btSocket.getInputStream();
             inputStream.skip(inputStream.available());
-
-            for (int i = 0; i < 26; i++) {
-
-                byte b = (byte) inputStream.read();
-                System.out.println((char) b);
-
-            }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -80,21 +73,20 @@ public class MainActivity extends AppCompatActivity {
     //Without ESP: [E0:62:67:47:19:AD, 55:0F:63:31:54:3D, FC:58:FA:2F:4C:2C, AA:F0:36:2F:B6:63, 80:57:19:E8:43:8F, 41:42:4C:F8:B4:A0, 58:FB:84:86:29:04, FF:FF:38:59:B5:B3, FC:58:FA:4B:E8:7F]
     //ESP MAC: 78:21:84:E0:B8:CA
 
-    public void initButtons(OutputStream outputStream ) {
-        createButtonListener((Button) findViewById(R.id.button1), outputStream, 49);
-        createButtonListener((Button) findViewById(R.id.button2), outputStream, 50);
-        createButtonListener((Button) findViewById(R.id.button3), outputStream, 51);
-        createButtonListener((Button) findViewById(R.id.button4), outputStream, 52);
+    public void initButtons(OutputStream outputStream) {
+        createButtonListener(findViewById(R.id.button1), outputStream, 49);
+        createButtonListener(findViewById(R.id.button2), outputStream, 50);
+        createButtonListener(findViewById(R.id.button3), outputStream, 51);
     }
 
-    public void createButtonListener(Button btn, OutputStream outputStream, int message) {
+    public void createButtonListener(Button btn, OutputStream outputStream, int moronicMessage) {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 System.out.println("Clicked " + btn.getText());
-                System.out.println("Message " + message);
+                System.out.println("Message " + moronicMessage);
                 try {
-                    outputStream.write(message);
+                    outputStream.write(moronicMessage);
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
@@ -102,5 +94,5 @@ public class MainActivity extends AppCompatActivity {
 
 
         });
-        }
+    }
 }
