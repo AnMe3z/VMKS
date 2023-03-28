a5799097f567694753908a2b10b437f5f6461179
AnMe3z <prodeadlight@gmail.com>
2023-03-28 11:05:23 +0300

    up and down

--- code/stage1/servos/main/main.ino
+++ code/stage1/servos/main/main.ino
@@ -9,13 +9,13 @@ HCPCA9685 HCPCA9685(I2CAdd);
 
 //int servoRoms[12][12] = {{72, 180}, {22, 170}, {0, 0}, {10, 140}, {0, 180}, {0, 0}, {0, 70}, {35, 70}, {0, 0}, {10, 60}, {35, 140}, {0, 0}};
 //                      c             c           c            c           c           c           c             c
-int servoRoms[4][6] = {{40, 180, 90, 170, 0, 0}, {3, 120, 85, 135, 0, 0}, {0, 115, 90, 150, 0, 0}, {0, 115, 100, 180, 0, 0}};
+int servoRoms[4][6] = {{40, 180, 90, 150, 0, 0}, {3, 120, 85, 135, 0, 0}, {0, 115, 90, 150, 0, 0}, {0, 115, 100, 160, 0, 0}};
 //the legs need to be closed at the start of the program for the angles to be true
 int servoCurrentAngles[4][3] = {{40, 170, 0}, {3, 156, 0}, {0, 150, 0}, {0, 180, 0}};
 int leg[4][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {9, 10, 11}};
 
 int legLenght = 10;
-int i = 0;
+int height = 1;
 
 void setup() {
   //init the servo driver
@@ -27,10 +27,7 @@ void setup() {
   // - stand up
 
   //make the robot to sit down
-//  inverseKinematics(0, 0);
-  inverseKinematics(1, i);
-//  inverseKinematics(2, 0);
-//  inverseKinematics(3, 0);
+  standDown();
   
   // init pause
   delay(750);
@@ -38,18 +35,37 @@ void setup() {
 
 void loop() {
 
-  while(i < 15){
-    i++;
-    inverseKinematics(1, i);
-    delay(300);
-  }
-  while(i > 0){
-    i--;
-    inverseKinematics(1, i);
-    delay(300);
-  }
+  standUp();
+
+  standDown();
   
 }
+
+// --- movements
+
+void standUp(){
+  while(height < 15){
+    height++;
+    inverseKinematics(0, height);
+    inverseKinematics(1, height);
+    inverseKinematics(2, height);
+    inverseKinematics(3, height);
+    delay(300);
+  }
+}
+
+void standDown(){
+  while(height > 0){
+    height--;
+    inverseKinematics(0, height);
+    inverseKinematics(1, height);
+    inverseKinematics(2, height);
+    inverseKinematics(3, height);
+    delay(300);
+  }
+}
+
+// --- essensials
 
 void inverseKinematics(int legIndex, double torsoHeight){
   int a = legLenght;
