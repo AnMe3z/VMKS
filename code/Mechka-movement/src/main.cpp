611d39e5ee133a3663b464255382f8158d11d821
AnMe3z <prodeadlight@gmail.com>
2023-04-04 10:50:38 +0300

    ne e tam problema

--- code/Mechka-movement/src/main.cpp
+++ code/Mechka-movement/src/main.cpp
@@ -84,38 +84,38 @@ void walk(){
   double cy[4] = {y[0], y[1], y[2], y[3]};
 
   // ### test
-  inverseKinematics(1, 0.3090167660379004 ,  0.04894340951367282);
-  delay(500);
-  inverseKinematics(1, 0.5877850796390499 ,  0.19098288018502607 );
-  delay(500);
-  inverseKinematics(1, 0.809016884614894 ,  0.4122145966358034 );
-  delay(500);
-  inverseKinematics(1, 0.9510564668343406 ,  0.6909828534003644 );
-  delay(500);
-  inverseKinematics(1, 0.9999999999999911 ,  0.9999998666179054 );
-  delay(500);
-  inverseKinematics(1, 0.9510565492690153 ,  1.3090168928918176 );
-  delay(500);
-  inverseKinematics(1, 0.8090170414149617 ,  1.5877851875474425 );
-  delay(500);
-  inverseKinematics(1, 0.5877852954558248 ,  1.809016963014935 );
-  delay(500);
-  inverseKinematics(1, 0.3090170197457293 ,  1.9510565080516864 );
-  delay(500);
-  inverseKinematics(1, 0.0 ,  2.0 );
-  delay(500);
+  // inverseKinematics(1, 5.3090167660379 ,  0.04894340951367282);
+  // delay(500);
+  // inverseKinematics(1, 5.58778507963905 ,  0.19098288018502607);
+  // delay(500);
+  // inverseKinematics(1, 0.809016884614894 ,  0.4122145966358034 );
+  // delay(500);
+  // inverseKinematics(1, 0.9510564668343406 ,  0.6909828534003644 );
+  // delay(500);
+  // inverseKinematics(1, 0.9999999999999911 ,  0.9999998666179054 );
+  // delay(500);
+  // inverseKinematics(1, 0.9510565492690153 ,  1.3090168928918176 );
+  // delay(500);
+  // inverseKinematics(1, 0.8090170414149617 ,  1.5877851875474425 );
+  // delay(500);
+  // inverseKinematics(1, 0.5877852954558248 ,  1.809016963014935 );
+  // delay(500);
+  // inverseKinematics(1, 0.3090170197457293 ,  1.9510565080516864 );
+  // delay(500);
+  // inverseKinematics(1, 0.0 ,  2.0 );
+  // delay(500);
   // ### test
 
-  // while(angle > 0){
-  //   angle -= 180/archSteps;
+  while(angle > 0){
+    angle -= 180/archSteps;
 
-  //   x[1] = cx[1] + radius * cos((angle * 71) / 4068);
-  //   y[1] = cy[1] + radius * sin((angle * 71) / 4068);
-  //   inverseKinematics(1, y[1], x[1]);
+    x[1] = cx[1] + radius * cos((angle * 71) / 4068);
+    y[1] = cy[1] - radius * sin((angle * 71) / 4068);
+    inverseKinematics(1, y[1], x[1]);
 
-  //   x[2] = cx[2] + radius * cos((angle * 71) / 4068);
-  //   y[2] = cy[2] + radius * sin((angle * 71) / 4068);
-  //   inverseKinematics(2, y[2], x[2]);
+    x[2] = cx[2] + radius * cos((angle * 71) / 4068);
+    y[2] = cy[2] - radius * sin((angle * 71) / 4068);
+    inverseKinematics(2, y[2], x[2]);
 
     // if(count < 9 && rising){
     //   count++;
@@ -138,41 +138,41 @@ void walk(){
     //   count--;
     // }
 
-    // inverseKinematics(0, y[0], x[0]);
-    // inverseKinematics(3, y[3], x[3]);
+    inverseKinematics(0, y[0], x[0]);
+    inverseKinematics(3, y[3], x[3]);
 
-  //   delay(150);
-  // }
+    delay(150);
+  }
 
-  // delay(1000);
+  delay(1000);
   
-  // // reset the angle
-  // angle = 180;
+  // reset the angle
+  angle = 180;
 
-  // y[1] = height;
-  // y[2] = height;
+  y[1] = height;
+  y[2] = height;
   
-  // while(angle > 0){ // loops 18 times
-  //     angle -= 180/archSteps;
+  while(angle > 0){ // loops 18 times
+      angle -= 180/archSteps;
 
-  //     x[0] = cx[0] + radius * cos((angle * 71) / 4068);
-  //     y[0] = cy[0] + radius * sin((angle * 71) / 4068);
-  //     inverseKinematics(0, y[0], x[0]);
+      x[0] = cx[0] + radius * cos((angle * 71) / 4068);
+      y[0] = cy[0] - radius * sin((angle * 71) / 4068);
+      inverseKinematics(0, y[0], x[0]);
 
-  //     x[3] = cx[3] + radius * cos((angle * 71) / 4068);
-  //     y[3] = cy[3] + radius * sin((angle * 71) / 4068);
-  //     inverseKinematics(3, y[3], x[3]);
+      x[3] = cx[3] + radius * cos((angle * 71) / 4068);
+      y[3] = cy[3] - radius * sin((angle * 71) / 4068);
+      inverseKinematics(3, y[3], x[3]);
 
-  //     //leg drift
-  //     x[1] -= lDStep;
-  //     inverseKinematics(1, y[1], x[1]);
-  //     x[2] -= lDStep;
-  //     inverseKinematics(2, y[2], x[2]);
+      //leg drift
+      x[1] -= lDStep;
+      inverseKinematics(1, y[1], x[1]);
+      x[2] -= lDStep;
+      inverseKinematics(2, y[2], x[2]);
 
-  //   delay(150);
-  // }
+    delay(150);
+  }
   
-  // delay(1000);
+  delay(1000);
   
 }
 
