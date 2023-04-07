3d7b6b81ad8ee3b6d83d6bdaca342b32488d8e8c
AnMe3z <prodeadlight@gmail.com>
2023-04-07 22:24:21 +0300

    hip servos now lock

--- code/Mechka-movement/src/main.cpp
+++ code/Mechka-movement/src/main.cpp
@@ -9,7 +9,7 @@ HCPCA9685 HCPCA9685(I2CAdd);
 
 //int servoRoms[12][12] = {{72, 180}, {22, 170}, {0, 0}, {10, 140}, {0, 180}, {0, 0}, {0, 70}, {35, 70}, {0, 0}, {10, 60}, {35, 140}, {0, 0}};
 //                      c             c           c            c           c           c           c             c
-int servoRoms[4][6] = {{40, 180, 90, 150, 0, 0}, {3, 120, 85, 135, 0, 0}, {0, 115, 90, 150, 0, 0}, {0, 115, 100, 160, 0, 0}};
+int servoRoms[4][6] = {{40, 180, 90, 150, 35, 0}, {3, 120, 85, 135, 75, 0}, {0, 115, 90, 150, 85, 0}, {0, 115, 100, 160, 90, 0}};
 //the legs need to be closed at the start of the program for the angles to be true
 int servoCurrentAngles[4][3] = {{40, 170, 0}, {3, 156, 0}, {0, 150, 0}, {0, 180, 0}};
 int leg[4][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {9, 10, 11}};
@@ -54,47 +54,42 @@ void loop() {
   
   standUp(5);
 
-  // pitchTestMovement();
-
-  // xTestMovement();
-
-  walk(50);
-  // standUp(5);
-  y[0] = 5;
-  x[0] = 0;
-  y[1] = 5;
-  x[1] = 0;
-  y[2] = 5;
-  x[2] = 0;
-  y[3] = 5;
-  x[3] = 0;
-  inverseKinematics(0, y[0], x[0]);
-  inverseKinematics(1, y[1], x[1]);
-  inverseKinematics(2, y[2], x[2]);
-  inverseKinematics(3, y[3], x[3]);
-  delay(150);
-
-  walk(50);
-  
-  y[0] = 5;
-  x[0] = 0;
-  y[1] = 5;
-  x[1] = 0;
-  y[2] = 5;
-  x[2] = 0;
-  y[3] = 5;
-  x[3] = 0;
-  inverseKinematics(0, y[0], x[0]);
-  inverseKinematics(1, y[1], x[1]);
-  inverseKinematics(2, y[2], x[2]);
-  inverseKinematics(3, y[3], x[3]);
-  delay(150);
-  
-  walk(50);
   // walk(50);
-  delay(1000);
+  // y[0] = 5;
+  // x[0] = 0;
+  // y[1] = 5;
+  // x[1] = 0;
+  // y[2] = 5;
+  // x[2] = 0;
+  // y[3] = 5;
+  // x[3] = 0;
+  // inverseKinematics(0, y[0], x[0]);
+  // inverseKinematics(1, y[1], x[1]);
+  // inverseKinematics(2, y[2], x[2]);
+  // inverseKinematics(3, y[3], x[3]);
+  // delay(150);
 
-  standDown();
+  // walk(50);
+
+  // y[0] = 5;
+  // x[0] = 0;
+  // y[1] = 5;
+  // x[1] = 0;
+  // y[2] = 5;
+  // x[2] = 0;
+  // y[3] = 5;
+  // x[3] = 0;
+  // inverseKinematics(0, y[0], x[0]);
+  // inverseKinematics(1, y[1], x[1]);
+  // inverseKinematics(2, y[2], x[2]);
+  // inverseKinematics(3, y[3], x[3]);
+  // delay(150);
+  
+  // walk(50);
+  
+  // delay(1000);
+
+  // standDown();
   
 }
 
@@ -432,6 +427,14 @@ void inverseKinematics(int legIndex, double torsoHeight, double x){
   
   //femur
   HCPCA9685.Servo(femurServoIndex, map(femurMax - femurAngle, 0, 180, 1, 450));
+
+  //hip
+  int hipServoIndex = leg[legIndex][2];
+  //the value bellow is meant to be a temporary patch
+  //TODO: to set default hip servo pos to 90 degrees
+  int hipMiddleAngle = servoRoms[legIndex][4];
+  HCPCA9685.Servo(hipServoIndex, map(hipMiddleAngle, 0, 180, 1, 450));
+
 }
 
 double cosineTheorem(int a, int b, double c){
